1f322a637e1ab18e17c02006a80827f10f125f30
lmc2179 <louiscialdella@gmail.com>
2017-04-09 15:15:15 -0700

    added modelled probability visualization to demo

--- asymmetric_payoff/demo.py
+++ asymmetric_payoff/demo.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import pandas as pd
 from asymmetric_payoff.threshold_calculation import calculate_optimal_threshold
 
-n = 200
+n = 500
 X_t = np.concatenate((np.random.normal(-1, 1, n), np.random.normal(1, 1, n)), axis=0)
 X = X_t.reshape(-1, 1)
 y = [0]*n + [1]*n
@@ -15,12 +15,18 @@ df['X'] = X_t
 df['y'] = y
 
 sns.distplot(X_t[:n], color='r')
-sns.distplot(X_t[n:], color='g')
+sns.distplot(X_t[n:], color='b')
 plt.show()
 
 clf = LogisticRegression()
 clf.fit(X, y)
 y_pred = clf.predict_proba(X)[:,1]
+
+X_plot_t = np.linspace(-4.5, 4.5)
+y_plot_positive = clf.predict_proba(X_plot_t.reshape(-1, 1))
+plt.plot(X_plot_t, 1- y_plot_positive)
+plt.plot(X_plot_t, y_plot_positive)
+plt.show()
 
 accuracy_matrix = [[1, 0],
                    [0, 1]]
