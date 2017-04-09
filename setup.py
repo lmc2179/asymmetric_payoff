c1f3ec78ccbb1d29be77a03ae3f5189f7ea7e11f
lmc2179 <louiscialdella@gmail.com>
2017-04-09 14:56:02 -0700

    added setup.py and travis support

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='asymmetric_payoff',
+      version='0.1',
+      description='tools for binary classifiers with asymmetric payoffs',
+      author='Louis Cialdella',
+      author_email='louiscialdella@gmail.com',
+      url='https://github.com/lmc2179/asymmetric_payoff',
+      packages=['asymmetric_payoff'],
+     )
