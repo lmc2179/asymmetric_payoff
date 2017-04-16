1bc72d459c5680eb2a67a9606cea1cbfad3070f1
lmc2179 <louiscialdella@gmail.com>
2017-04-16 12:48:39 -0700

    wrapped threshold calculation in class

--- asymmetric_payoff/tests/test_calculation.py
+++ asymmetric_payoff/tests/test_calculation.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from asymmetric_payoff.threshold_calculation import calculate_optimal_threshold, AsymmetricPayoffClassifier
+from asymmetric_payoff.threshold_calculation import OptimalPayoffThreshold, AsymmetricPayoffClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
@@ -10,15 +10,15 @@ ACCURACY_MATRIX = np.array([[1, 0], [0, 1]])
 
 class ThresholdCalculationTest(unittest.TestCase):
     def test_accuracy(self):
-        t = calculate_optimal_threshold(np.array([0, 0, 1, 1]),
-                                        np.array([0.2, 0.4, 0.45, 0.8]),
-                                        ACCURACY_MATRIX)
+        t = OptimalPayoffThreshold(np.array([0, 0, 1, 1]),
+                                    np.array([0.2, 0.4, 0.45, 0.8]),
+                                    ACCURACY_MATRIX).calculate_optimal_threshold()
         self.assertEqual(t, 0.45)
 
     def test_accuracy_non_unique(self):
-        t = calculate_optimal_threshold(np.array([0, 1, 1, 1]),
+        t = OptimalPayoffThreshold(np.array([0, 1, 1, 1]),
                                         np.array([0.2, 0.4, 0.4, 0.8]),
-                                        ACCURACY_MATRIX)
+                                        ACCURACY_MATRIX).calculate_optimal_threshold()
         self.assertEqual(t, 0.40)
 
 class WrapperTest(unittest.TestCase):
--- asymmetric_payoff/threshold_calculation.py
+++ asymmetric_payoff/threshold_calculation.py
@@ -1,29 +1,33 @@
 import numpy as np
 
-def calculate_optimal_threshold(y, y_predicted, payoff_matrix):
-    """"""
-    y_pred_sorted, y_sorted = zip(*sorted(zip(y_predicted, y)))
-    zero_transition_cost = payoff_matrix[0][0] - payoff_matrix[0][1]
-    one_transition_cost = payoff_matrix[1][0] - payoff_matrix[1][1]
-    possible_thresholds = sorted(list(set(y_pred_sorted)))
-    threshold_position = {t: i for i,t in enumerate(possible_thresholds)}
-    y_0, y_1 = np.zeros(len(possible_thresholds)), np.zeros(len(possible_thresholds)) # TODO: Better name
-    true_counts = [y_0, y_1]
-    for y_i, t in zip(y, y_predicted):
-        true_counts[y_i][threshold_position[t]] += 1
-    initial_utility = sum(true_counts[0] * payoff_matrix[0][1] \
-                          + true_counts[1] * payoff_matrix[1][1])
-    best_utility = initial_utility
-    best_threshold = 0
-    current_utility = initial_utility
-    for i, t in enumerate(possible_thresholds):
-        new_utility = true_counts[0][i-1] * zero_transition_cost \
-                    + true_counts[1][i-1] * one_transition_cost \
-                    + current_utility
-        if new_utility >= best_utility:
-            best_utility, best_threshold = new_utility, t
-        current_utility = new_utility
-    return best_threshold
+class OptimalPayoffThreshold(object):
+    def __init__(self, y, y_predicted, payoff_matrix):
+        self.y, self.y_predicted, self.payoff_matrix = y, y_predicted, payoff_matrix
+
+    def calculate_optimal_threshold(self):
+        """"""
+        y_pred_sorted, y_sorted = zip(*sorted(zip(self.y_predicted, self.y)))
+        zero_transition_cost = self.payoff_matrix[0][0] - self.payoff_matrix[0][1]
+        one_transition_cost = self.payoff_matrix[1][0] - self.payoff_matrix[1][1]
+        possible_thresholds = sorted(list(set(y_pred_sorted)))
+        threshold_position = {t: i for i,t in enumerate(possible_thresholds)}
+        y_0, y_1 = np.zeros(len(possible_thresholds)), np.zeros(len(possible_thresholds)) # TODO: Better name
+        true_counts = [y_0, y_1]
+        for y_i, t in zip(self.y, self.y_predicted):
+            true_counts[y_i][threshold_position[t]] += 1
+        initial_utility = sum(true_counts[0] * self.payoff_matrix[0][1] \
+                              + true_counts[1] * self.payoff_matrix[1][1])
+        best_utility = initial_utility
+        best_threshold = 0
+        current_utility = initial_utility
+        for i, t in enumerate(possible_thresholds):
+            new_utility = true_counts[0][i-1] * zero_transition_cost \
+                        + true_counts[1][i-1] * one_transition_cost \
+                        + current_utility
+            if new_utility >= best_utility:
+                best_utility, best_threshold = new_utility, t
+            current_utility = new_utility
+        return best_threshold
 
 class AsymmetricPayoffClassifier(object):
     def __init__(self, base_model, payoff_matrix):
@@ -34,7 +38,7 @@ class AsymmetricPayoffClassifier(object):
     def fit(self, X, y):
         self.model.fit(X, y)
         y_predicted = self.model.predict_proba(X)[:,1]
-        self.threshold_ = calculate_optimal_threshold(y, y_predicted, self.payoff_matrix)
+        self.threshold_ = OptimalPayoffThreshold(y, y_predicted, self.payoff_matrix).calculate_optimal_threshold()
 
     def predict(self, X):
         y_predicted_proba = self.model.predict_proba(X)[:,1]
